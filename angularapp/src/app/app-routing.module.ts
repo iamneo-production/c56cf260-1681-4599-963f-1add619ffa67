84c4032bc0cda33da7dbd9a980e7f6117ca65ee5
anudeep586 <anudeep4n@gmail.com>
2021-10-19 14:07:45 +0000

    files added

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { RegisterationComponent } from './registeration/registeration.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { AddimageComponent } from './addimage/addimage.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path:"register",component:RegisterationComponent
+    
+  },
+  {
+    path:"",component:LoginComponent
+  },
+  {
+    path:"home",component:HomeComponent
+  },
+  {
+    path:"adddimage",component:AddimageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+
+
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,15 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterationComponent } from './registeration/registeration.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterationComponent,
+    LoginComponent,
+   
   ],
   imports: [
-    BrowserModule,
+  
+  BrowserModule,
+  FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
